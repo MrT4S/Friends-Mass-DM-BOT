f374fb945a58681b6988e625d7945676fb6b0f19
Tezzy <65974842+Tezzy-lab@users.noreply.github.com>
2021-01-05 19:55:24 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 token="Mzc2MjE1NTU4ODA4NTM1MDQx.X_D6Kg.VnHrajmD4pixyx-7Lw6CSqZr4W8"
 # TOKEN ^^^
 dm_message="**__JOIN UP NEW SERVER__**\nhttps://discord.gg/dreams"
-# DM MESSAGE ^^^
+# DM MESSAGE ^^^ ; use \n to go next line
