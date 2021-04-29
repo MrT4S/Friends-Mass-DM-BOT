2c071dd8392040cd31a160811fd9746ca20b31b2
Daddy_Tezzy <65974842+DaddyTezzy@users.noreply.github.com>
2021-04-29 15:25:35 -0400

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-token="Mzc2MjE1NTU4ODA4NTM1MDQx.X_D6Kg.VnHrajmD4pixyx-7Lw6CSqZr4W8"
-# TOKEN ^^^
-dm_message="**__JOIN UP NEW SERVER__**\nhttps://discord.gg/lobby"
-# DM MESSAGE ^^^ ; use \n to go next line
